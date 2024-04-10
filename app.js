24577192bf20b367094c356ce3baacc1f1c1f42c
Mehran Roham <mehranroham@gmail.com>
2024-04-10 04:18:05 +0330

    ways of create arrays

---
+++ app.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+// const newNumber = new Array(4, 'hi');
+// console.log(newNumber);
+
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
+
+const numbers2 = Array.from('hi mehran');
+console.log(numbers2);
+
+const listItems = document.querySelectorAll('li');
+console.log(listItems);
