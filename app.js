a8862ded51b7aaac98f93236ac634d77dd7be863
Mehran Roham <mehranroham@gmail.com>
2024-04-10 05:01:33 +0330

    splice method

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
-const numbers = [1, 2, 3];
-console.log(numbers);
+// const numbers = [1, 2, 3];
+// console.log(numbers);
 
-// const newNumber = new Array(4, 'hi');
-// console.log(newNumber);
+// // const newNumber = new Array(4, 'hi');
+// // console.log(newNumber);
 
-// const yetMoreNumbers = Array.of(1, 2);
-// console.log(yetMoreNumbers);
+// // const yetMoreNumbers = Array.of(1, 2);
+// // console.log(yetMoreNumbers);
 
-const numbers2 = Array.from('hi mehran');
-console.log(numbers2);
+// const numbers2 = Array.from('hi mehran');
+// console.log(numbers2);
 
-const listItems = document.querySelectorAll('li');
-console.log(listItems);
+// const listItems = document.querySelectorAll('li');
+// console.log(listItems);
+
+// const array = [
+//   [1, 2, 3],
+//   [-5.6, -1, 1000, 22],
+// ];
+
+// for (const data of array) {
+//   for (const cl of data) {
+//     console.log(cl);
+//   }
+// }
+
+const hobbies = ['sport', 'tv'];
+
+hobbies.push('coding');
+hobbies.unshift('1');
+hobbies.pop(); // pop from the end
+hobbies.shift(); // remove from the beggining
+
+console.log(hobbies);
+
+hobbies[10] = 'test'; // we will get empty places
+
+console.log(hobbies);
+
+const array = [1, 2, 3, 4];
+array.splice(1, 1, 'hi', 'good'); // starting element , delete as many we want , insert elements
+console.log(array);
+
+// array.splice(2); // delete all from the starting element
+// console.log(array);
+
+array.splice(-1, 1);
+console.log(array);
